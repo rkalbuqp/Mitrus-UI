a91dcee7fa835f67469122ea8c64101ec2309cd0
Kassia <kassiademaria@gmail.com>
2026-06-13 14:32:37 -0300

    feat(atoms): add barrel export for atomic components and their types

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,47 @@
+export { default as Avatar } from "./Avatar";
+export type { AvatarProps, AvatarShape, AvatarSize, AvatarStatus } from "./Avatar";
+
+export { default as Badge } from "./Badge";
+export type { BadgeAppearance, BadgeProps, BadgeSize, BadgeVariant } from "./Badge";
+
+export { default as Button } from "./Button";
+export type { ButtonProps, ButtonSize, ButtonVariant } from "./Button";
+
+export { default as Checkbox } from "./Checkbox";
+export type { CheckboxProps, CheckboxSize } from "./Checkbox";
+
+export { default as Icon } from "./Icon";
+export type { IconProps, IconSize, IconVariant } from "./Icon";
+
+export { default as Image } from "./Image";
+export type { ImageFit, ImageProps, ImageRatio, ImageRounded, ImageSize } from "./Image";
+
+export { default as Label } from "./Label";
+export type { LabelProps, LabelSize } from "./Label";
+
+export { default as Link } from "./Link";
+export type { LinkProps, LinkSize, LinkUnderline, LinkVariant } from "./Link";
+
+export { default as RadioButton } from "./RadioButton";
+export type { RadioButtonProps, RadioButtonSize } from "./RadioButton";
+
+export { default as Select } from "./Select";
+export type { SelectOption, SelectProps, SelectSize } from "./Select";
+
+export { default as Skeleton } from "./Skeleton";
+export type { SkeletonProps, SkeletonSize, SkeletonVariant } from "./Skeleton";
+
+export { default as Switch } from "./Switch";
+export type { SwitchProps, SwitchSize } from "./Switch";
+
+export { default as Tag } from "./Tag";
+export type { TagProps, TagSize, TagVariant } from "./Tag";
+
+export { default as Text } from "./Text";
+export type { TextAlign, TextSize, TextTone, TextWeight } from "./Text";
+
+export { default as TextArea } from "./TextArea";
+export type { TextAreaProps, TextAreaResize, TextAreaSize } from "./TextArea";
+
+export { default as Tooltip } from "./Tooltip";
+export type { TooltipPlacement, TooltipProps, TooltipSize } from "./Tooltip";
